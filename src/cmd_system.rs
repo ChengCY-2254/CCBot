ecbff206047a85b45d18b87789adfd800a6fe0b5
Cheng <cheng@hutao.run>
2025-05-22 23:32:56 +0800

    拆分杂乱的模块，移除了一些用不到的参数。例如GuildChannel。

--- src/cmd_system.rs
+++ src/cmd_system.rs
@@ -1,19 +1,17 @@
 //! This file contains the implementation of the HubSystem struct and its associated methods.
 //! `[#poise::command]`中的`#[channel_types]`对应路径为[serenity::model::channel::ChannelType] Enum
 
-use crate::HttpKey;
-use anyhow::Context;
+mod music;
+use crate::cmd_system::music::{join, leave, play_music, stop};
 use chrono::FixedOffset;
 use futures::{Stream, StreamExt};
 use lazy_static::lazy_static;
 use poise::CreateReply;
-use serenity::all::{ActivityData, GuildChannel, MessageBuilder};
-use serenity::async_trait;
-use songbird::input::YoutubeDl;
-use songbird::{Event, EventContext, EventHandler, TrackEvent};
+use serenity::all::{ActivityData, MessageBuilder};
 use std::ops::Deref;
 
 lazy_static! {
+    /// UTC+8时区计算
     pub static ref UTC8: FixedOffset = FixedOffset::east_opt(8 * 3600).unwrap();
 }
 
@@ -24,15 +22,15 @@ type Error = anyhow::Error;
 ///上下文类型
 type PoiseContext<'a> = poise::Context<'a, Data, Error>;
 
-#[poise::command(slash_command, prefix_command)]
+#[poise::command(slash_command, prefix_command,context_menu_command="用户信息")]
 pub async fn ping(
     ctx: PoiseContext<'_>,
-    #[description = "选择一个用户"] user: Option<poise::serenity_prelude::User>,
+    #[description = "选择一个用户"] user: poise::serenity_prelude::User,
 ) -> crate::Result<()> {
-    let u = user.as_ref().unwrap_or_else(|| ctx.author());
+    let u = user;
 
     let response = MessageBuilder::new()
-        .mention(u)
+        .mention(&u)
         .push("创建于")
         .push_bold_safe(ToString::to_string(
             &u.created_at()
@@ -48,7 +46,7 @@ pub async fn ping(
 
 /// 注册命令的命令，需要使用@`[yourbot]` register来进行使用
 #[poise::command(prefix_command, aliases("reg"))]
-pub async fn register(ctx: PoiseContext<'_>) -> Result<(), Error> {
+pub async fn register(ctx: PoiseContext<'_>) -> crate::Result<()> {
     poise::builtins::register_application_commands_buttons(ctx).await?;
     Ok(())
 }
@@ -57,8 +55,9 @@ pub async fn register(ctx: PoiseContext<'_>) -> Result<(), Error> {
 #[poise::command(
     slash_command,
     aliases("status"),
-    required_permissions = "ADMINISTRATOR"
+    required_permissions = "ADMINISTRATOR",
 )]
+/// 设置机器人状态
 pub async fn set_status(
     ctx: PoiseContext<'_>,
     #[autocomplete = "autocomplete_activity_type"]
@@ -66,7 +65,7 @@ pub async fn set_status(
     activity_type: String,
     #[description = "内容"] activity_name: String,
     #[description = "活动网址"] url: Option<String>,
-) -> Result<(), Error> {
+) -> crate::Result<()> {
     {
         let activity_name = activity_name.clone();
         match activity_type.as_str() {
@@ -111,144 +110,7 @@ pub async fn autocomplete_activity_type(
         .map(|name| name.to_string())
 }
 
-/// 加入语音频道
-#[poise::command(slash_command)]
-pub async fn play_music(
-    ctx: PoiseContext<'_>,
-    #[description = "语音频道"]
-    #[channel_types("Voice")]
-    channel: GuildChannel,
-    #[description = "播放链接，支持BiliBili"] url: String,
-) -> Result<(), Error> {
-    let guild_id = channel.guild_id;
-
-    let http_client = ctx
-        .serenity_context()
-        .data
-        .read()
-        .await
-        .get::<HttpKey>()
-        .cloned()
-        .with_context(|| "get http client failed")?;
-    log::info!("http客户端获取成功");
-
-    let manager = songbird::get(ctx.serenity_context())
-        .await
-        .with_context(|| "获取语音客户端失败")?
-        .clone();
-    log::info!("获取语音客户端成功");
-    // 加入语音频道
-    if let Some(handler_lock) = manager.get(guild_id) {
-        let mut handler = handler_lock.lock().await;
-
-        log::info!("获取语音频道成功，即将开始推流");
-        let src = YoutubeDl::new(http_client, url);
-
-        log::info!("获取YoutubeDl成功");
-
-        let _trace = handler.play_input(src.clone().into());
-
-        ctx.say("开始播放".to_string()).await?;
-        return Ok(());
-    }
-    Err(anyhow::anyhow!("出现异常，无法播放。"))
-}
-
-#[poise::command(slash_command, required_permissions = "ADMINISTRATOR")]
-async fn join(
-    ctx: PoiseContext<'_>,
-    #[channel_types("Voice")] channel: GuildChannel,
-) -> crate::Result<()> {
-    let (guild_id, channel_id) = {
-        let guild = channel.guild_id;
-        let channel_id = channel.id;
-        (guild, channel_id)
-    };
-
-    let manager = songbird::get(ctx.serenity_context())
-        .await
-        .with_context(|| "语音客户端初始化中")?
-        .clone();
-    let handler_lock = manager.join(guild_id, channel_id).await?;
-    let mut handler = handler_lock.lock().await;
-    handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
-    let reply = CreateReply::default()
-        .ephemeral(true)
-        .content(format!("已加入语音频道: {}", channel.name));
-    ctx.send(reply).await?;
-    Ok(())
-}
-
-struct TrackErrorNotifier;
-
-#[async_trait]
-impl EventHandler for TrackErrorNotifier {
-    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
-        if let EventContext::Track(track_list) = ctx {
-            for (state, handle) in *track_list {
-                println!(
-                    "Track {:?} encountered an error: {:?}",
-                    handle.uuid(),
-                    state.playing
-                );
-            }
-        }
-
-        None
-    }
-}
-
-#[poise::command(slash_command, required_permissions = "ADMINISTRATOR")]
-async fn leave(
-    ctx: PoiseContext<'_>,
-    #[channel_types("Voice")] channel: GuildChannel,
-) -> crate::Result<()> {
-    let guild_id = channel.guild_id;
-    let manager = songbird::get(ctx.serenity_context())
-        .await
-        .with_context(|| "语音客户端初始化中")?
-        .clone();
-
-    let has_handler = manager.get(guild_id).is_some();
-
-    if has_handler {
-        if let Err(why) = manager.remove(guild_id).await {
-            log::error!("Error leaving channel: {:?}", why);
-            ctx.say("离开语音频道失败").await?;
-            return Err(anyhow::anyhow!("离开语音频道失败"));
-        };
-    }
-    ctx.send(
-        CreateReply::default()
-            .ephemeral(true)
-            .content(format!("已离开频道 {}", channel.name)),
-    )
-    .await?;
-    Ok(())
-}
-
-#[poise::command(slash_command, required_permissions = "ADMINISTRATOR")]
-async fn stop(
-    ctx: PoiseContext<'_>,
-    #[channel_types("Voice")] channel: GuildChannel,
-) -> crate::Result<()> {
-    let guild_id = channel.guild_id;
-    let manager = songbird::get(ctx.serenity_context())
-        .await
-        .with_context(|| "语音客户端初始化中")?
-        .clone();
-
-    let cell = manager.get(guild_id).with_context(|| "找不到对应的频道")?;
-    {
-        let mut handler = cell.lock().await;
-        handler.stop();
-        let response = CreateReply::default().content("已停止播放").ephemeral(true);
-        ctx.send(response).await?;
-    }
-
-    Ok(())
-}
-
+/// 命令行框架程序
 pub fn frame_work() -> poise::Framework<Data, Error> {
     log::info!("create framework");
     let framework: poise::Framework<Data, anyhow::Error> = poise::Framework::builder()
---
+++ src/cmd_system/music.rs
@@ -0,0 +1,184 @@
+//! 语音模块
+
+use crate::HttpKey;
+use crate::cmd_system::{Error, PoiseContext};
+use anyhow::Context;
+use lazy_static::lazy_static;
+use poise::{CreateReply, async_trait};
+use serenity::all::GuildChannel;
+use songbird::input::YoutubeDl;
+use songbird::{Event, EventContext, EventHandler, TrackEvent};
+
+lazy_static! {
+    /// 音乐播放的控制队列
+    pub static ref MUSIC_QUEUE: tokio::sync::RwLock<Vec<String>> = tokio::sync::RwLock::new(vec![]);
+    ///播放状态，false为没有播放，true为播放
+    pub static ref PLAY_STATE: tokio::sync::RwLock<bool> = tokio::sync::RwLock::new(false);
+}
+
+/// 播放音乐或视频，可播放网站以yt-dlp支持的网站为准
+#[poise::command(slash_command)]
+pub(super) async fn play_music(
+    ctx: PoiseContext<'_>,
+    #[description = "播放链接，支持BiliBili，更多网站请参见yt-dlp开源项目的支持列表"] url: String,
+) -> Result<(), Error> {
+    let guild_id = ctx.guild_id().with_context(|| "没有在服务器中")?;
+    {
+        let queue = MUSIC_QUEUE.read().await;
+        if queue.contains(&url) {
+            ctx.send(
+                CreateReply::default()
+                    .ephemeral(true)
+                    .content("该链接已经在播放队列中"),
+            )
+            .await?;
+            return Ok(());
+        }
+    }
+    {
+        let mut queue = MUSIC_QUEUE.write().await;
+        queue.push(url.clone());
+        log::info!("当前播放队列：{:?}", *queue);
+    }
+    {
+        let state = PLAY_STATE.read().await;
+        if *state {
+            ctx.send(
+                CreateReply::default()
+                    .ephemeral(true)
+                    .content("正在播放中，请稍后再试"),
+            )
+            .await?;
+            return Ok(());
+        }
+    }
+    let http_client = ctx
+        .serenity_context()
+        .data
+        .read()
+        .await
+        .get::<HttpKey>()
+        .cloned()
+        .with_context(|| "get http client failed")?;
+    log::info!("http客户端获取成功");
+
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .with_context(|| "获取语音客户端失败")?
+        .clone();
+    log::info!("获取语音客户端成功");
+    // 加入语音频道
+    if let Some(handler_lock) = manager.get(guild_id) {
+        let mut handler = handler_lock.lock().await;
+
+        log::info!("获取语音频道成功，即将开始推流");
+        let src = YoutubeDl::new(http_client, url);
+
+        log::info!("获取YoutubeDl成功");
+
+        let _ = handler.play_input(src.clone().into());
+
+        ctx.say("开始播放".to_string()).await?;
+        return Ok(());
+    }
+    Err(anyhow::anyhow!("出现异常，无法播放。"))
+}
+
+///加入一个频道
+#[poise::command(slash_command, required_permissions = "ADMINISTRATOR")]
+pub(super) async fn join(
+    ctx: PoiseContext<'_>,
+    #[channel_types("Voice")] channel: GuildChannel,
+) -> crate::Result<()> {
+    let (guild_id, channel_id) = {
+        let guild = channel.guild_id;
+        let channel_id = channel.id;
+        (guild, channel_id)
+    };
+
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .with_context(|| "语音客户端初始化中")?
+        .clone();
+    let handler_lock = manager.join(guild_id, channel_id).await?;
+    let mut handler = handler_lock.lock().await;
+    handler.add_global_event(TrackEvent::Error.into(), TrackErrorNotifier);
+    // handler.add_global_event(TrackEvent::End.into(), PlayEnd);
+    // handler.add_global_event(TrackEvent::Playable.into(), PlayStart);
+    let reply = CreateReply::default()
+        .ephemeral(true)
+        .content(format!("已加入语音频道: {}", channel.name));
+    ctx.send(reply).await?;
+    Ok(())
+}
+
+pub(super) struct TrackErrorNotifier;
+// /// 检查播放状态
+// pub(super) struct PlayEnd;
+// /// 检查播放状态
+// pub(super) struct PlayStart;
+
+#[async_trait]
+impl EventHandler for TrackErrorNotifier {
+    async fn act(&self, ctx: &EventContext<'_>) -> Option<Event> {
+        if let EventContext::Track(track_list) = ctx {
+            for (state, handle) in *track_list {
+                println!(
+                    "Track {:?} encountered an error: {:?}",
+                    handle.uuid(),
+                    state.playing
+                );
+            }
+        }
+
+        None
+    }
+}
+
+#[poise::command(slash_command, required_permissions = "ADMINISTRATOR")]
+pub(super) async fn leave(
+    ctx: PoiseContext<'_>,
+    #[channel_types("Voice")] channel: GuildChannel,
+) -> crate::Result<()> {
+    let guild_id = channel.guild_id;
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .with_context(|| "语音客户端初始化中")?
+        .clone();
+
+    let has_handler = manager.get(guild_id).is_some();
+
+    if has_handler {
+        if let Err(why) = manager.remove(guild_id).await {
+            log::error!("Error leaving channel: {:?}", why);
+            ctx.say("离开语音频道失败").await?;
+            return Err(anyhow::anyhow!("离开语音频道失败"));
+        };
+    }
+    ctx.send(
+        CreateReply::default()
+            .ephemeral(true)
+            .content(format!("已离开频道 {}", channel.name)),
+    )
+    .await?;
+    Ok(())
+}
+
+#[poise::command(slash_command, required_permissions = "ADMINISTRATOR")]
+pub(super) async fn stop(ctx: PoiseContext<'_>) -> crate::Result<()> {
+    let guild_id = ctx.guild_id().with_context(|| "没有在服务器中")?;
+    let manager = songbird::get(ctx.serenity_context())
+        .await
+        .with_context(|| "语音客户端初始化中")?
+        .clone();
+
+    let cell = manager.get(guild_id).with_context(|| "找不到对应的频道")?;
+    {
+        let mut handler = cell.lock().await;
+        handler.stop();
+        let response = CreateReply::default().content("已停止播放").ephemeral(true);
+        ctx.send(response).await?;
+    }
+
+    Ok(())
+}
