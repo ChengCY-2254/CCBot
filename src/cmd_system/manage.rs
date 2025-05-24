ad06148d0fa4119b4f473074ae53a3435a96bedd
Cheng <cheng@hutao.run>
2025-05-24 15:11:03 +0800

    feat: refactor withdraw command to include add and remove subcommands for channel management

--- src/cmd_system/manage.rs
+++ src/cmd_system/manage.rs
@@ -2,26 +2,26 @@
 //! 首先需要的是将xx频道加入撤回列表  
 //! 那么我需要的是add withdraw #channelID和remove withdraw #channelID  
 //! 需要查看subcommand的写法[link](https://github.com/serenity-rs/poise/blob/current/examples/feature_showcase/subcommand_required.rs)
+//! 吃了个大亏，应该把add放到withdraw的子命令中，而不是放在顶层，也就是 withdraw add #channelID
 
 use crate::keys::BotDataKey;
 use crate::{ExportVec, PoiseContext, create_ephemeral_reply};
-use anyhow::Context;
+use anyhow::{Context, anyhow};
 use serenity::all::GuildChannel;
 
 #[poise::command(
     slash_command,
-    subcommands("withdraw"),
+    subcommands("add", "remove"),
     subcommand_required,
     required_permissions = "ADMINISTRATOR",
     prefix_command
 )]
-pub async fn add(_ctx: PoiseContext<'_>) -> crate::Result<()> {
+pub async fn withdraw(_ctx: PoiseContext<'_>) -> crate::Result<()> {
     Ok(())
 }
 
-/// 监控一个频道，对其中的消息进行撤回。
-#[poise::command(slash_command, required_permissions = "ADMINISTRATOR", prefix_command)]
-pub async fn withdraw(
+#[poise::command(slash_command, prefix_command)]
+pub async fn add(
     ctx: PoiseContext<'_>,
     #[description = "频道"] channel: Option<GuildChannel>,
 ) -> crate::Result<()> {
@@ -30,22 +30,73 @@ pub async fn withdraw(
     }
     Ok(())
 }
+#[poise::command(slash_command, prefix_command)]
+pub async fn remove(
+    ctx: PoiseContext<'_>,
+    #[description = "频道"] channel: Option<GuildChannel>,
+) -> crate::Result<()> {
+    if let Some(channel) = channel {
+        handle_remove(ctx, channel).await?;
+    }
+    Ok(())
+}
 
-/// 处理添加频道的逻辑
-pub async fn handle_add(ctx: PoiseContext<'_>, channel: GuildChannel) -> crate::Result<()> {
-    let add_monitored_channel = Box::pin(async || {
+async fn handle_add(ctx: PoiseContext<'_>, channel: GuildChannel) -> crate::Result<()> {
+    let (already_exists, channel_name) = {
         let type_map = ctx.serenity_context().data.write().await;
         let data = type_map.get::<BotDataKey>();
         let mut data = data.context("app数据目录访问失败")?.exclusive_access();
-        data.add_monitored_channel(channel.id);
-        data.save("config/")
-    });
-    add_monitored_channel().await?;
-    let response = create_ephemeral_reply(format!("已将频道 {} 添加到撤回列表", channel.name));
-    ctx.send(response).await?;
+        let exists = data.monitored_channels.contains(&channel.id);
+        let name = channel.name.clone();
+        if !exists {
+            data.add_monitored_channel(channel.id);
+            data.save("config/")?;
+        }
+        (exists, name)
+    };
+
+    if already_exists {
+        let response = create_ephemeral_reply(format!(
+            "频道 {}:{} 已经在撤回列表中",
+            channel_name, channel.id
+        ));
+        ctx.send(response).await?;
+    } else {
+        let response = create_ephemeral_reply(format!("已将频道 {} 添加到撤回列表", channel_name));
+        ctx.send(response).await.map_err(|why| anyhow!("{}", why))?;
+    }
+    Ok(())
+}
+async fn handle_remove(ctx: PoiseContext<'_>, channel: GuildChannel) -> crate::Result<()> {
+    let (exists, channel_name) = {
+        let type_map = ctx.serenity_context().data.write().await;
+        let data = type_map.get::<BotDataKey>();
+        let mut data = data.context("app数据目录访问失败")?.exclusive_access();
+        let exists = data.monitored_channels.contains(&channel.id);
+        let name = channel.name.clone();
+        if exists {
+            data.remove_monitored_channel(channel.id);
+            data.save("config/")?;
+        }
+        (exists, name)
+    };
+
+    if exists {
+        let response = create_ephemeral_reply(format!(
+            "已将频道 {}:{} 从撤回列表中移除",
+            channel_name, channel.id
+        ));
+        ctx.send(response).await?;
+    } else {
+        let response = create_ephemeral_reply(format!(
+            "频道 {}:{} 不在撤回列表中",
+            channel_name, channel.id
+        ));
+        ctx.send(response).await?;
+    }
     Ok(())
 }
 
 pub fn manage_export() -> ExportVec {
-    vec![add()]
+    vec![withdraw()]
 }
