2ca15ea03f0b17bfc9c1f6c35a6098aa1ad9ca34
Cheng <cheng@hutao.run>
2025-05-27 01:00:52 +0800

    fix:添加clear里的消息删除计数，而不是直接返回消息总数。干掉sql依赖，毕竟我讨厌sql。

--- src/keys.rs
+++ src/keys.rs
@@ -14,9 +14,10 @@ impl TypeMapKey for BotDataKey {
     type Value = crate::model::Data;
 }
 /// 机器人的数据库Key
+#[cfg(feature = "db")]
 #[allow(unused)]
 pub struct BotDB;
-
+#[cfg(feature = "db")]
 impl TypeMapKey for BotDB {
     type Value = sqlx::SqlitePool;
 }
