6d6fda0fcdd9f81a0baa0ad17e2a41e6f9508a4d
Cheng <cheng@hutao.run>
2025-05-31 11:54:44 +0800

    refactor(music): 优化离开语音频道功能并添加检查
    - 添加检查当前是否加入语音频道的逻辑
    - 如果未加入任何语音频道，发送相应提示消息

--- src/cmd_system/music.rs
+++ src/cmd_system/music.rs
@@ -2,7 +2,7 @@
 //! 语音方向的内容
 
 use crate::cmd_system::utils::get_http_client;
-use crate::utils::UpSafeCell;
+use crate::utils::{create_ephemeral_reply, UpSafeCell};
 use crate::{CommandVec, PoiseContext};
 use anyhow::{Context, anyhow};
 use lazy_static::lazy_static;
@@ -41,7 +41,7 @@ pub async fn play(
     if let Some(handler_lock) = manager.get(guild_id) {
         let mut handler = handler_lock.lock().await;
         log::info!("获取语音频道成功，正在搜索内容");
-        let (source_url, title) = if text.starts_with("https://")|| text.starts_with("http://") {
+        let (source_url, title) = if text.starts_with("https://") || text.starts_with("http://") {
             let data = YoutubeDl::new(http_client.clone(), text)
                 .aux_metadata()
                 .await?;
@@ -139,41 +139,45 @@ pub async fn join(
 }
 
 /// 离开一个语音频道
+/// todo 使用上面存放的CURRENT_JOIN_CHANNEL进行离开操作
 #[poise::command(slash_command, owners_only)]
-pub async fn leave(
-    ctx: PoiseContext<'_>,
-    #[channel_types("Voice")] channel: GuildChannel,
-) -> crate::Result<()> {
-    let guild_id = channel.guild_id;
-    let manager = songbird::get(ctx.serenity_context())
-        .await
-        .with_context(|| "语音客户端初始化中")?
-        .clone();
+pub async fn leave(ctx: PoiseContext<'_>) -> crate::Result<()> {
+    let cur_channel = CURRENT_JOIN_CHANNEL.access().clone();
+    if let Some(channel) = cur_channel {
+        let guild_id = channel.guild_id;
+        let manager = songbird::get(ctx.serenity_context())
+            .await
+            .with_context(|| "语音客户端初始化中")?
+            .clone();
 
-    let has_handler = manager.get(guild_id).is_some();
+        let has_handler = manager.get(guild_id).is_some();
 
-    if has_handler {
-        if let Err(why) = manager.remove(guild_id).await {
-            log::error!("Error leaving channel: {:?}", why);
-            ctx.say("离开语音频道失败").await?;
-            return Err(anyhow::anyhow!("离开语音频道失败"));
-        };
-        // 暂停播放就重置状态
-        update_channel_state(ctx, "").await?;
-        {
-            // 离开了频道，所以可以丢弃值
-            let mut current_join_channel = CURRENT_JOIN_CHANNEL.exclusive_access();
-            if current_join_channel.is_some() {
-                let _ = current_join_channel.take();
+        if has_handler {
+            if let Err(why) = manager.remove(guild_id).await {
+                log::error!("Error leaving channel: {:?}", why);
+                ctx.say("离开语音频道失败").await?;
+                return Err(anyhow::anyhow!("离开语音频道失败"));
+            };
+            // 暂停播放就重置状态
+            update_channel_state(ctx, "").await?;
+            {
+                // 离开了频道，所以可以丢弃值
+                let mut current_join_channel = CURRENT_JOIN_CHANNEL.exclusive_access();
+                if current_join_channel.is_some() {
+                    let _ = current_join_channel.take();
+                }
             }
         }
+        ctx.send(
+            CreateReply::default()
+                .ephemeral(true)
+                .content(format!("已离开频道 {}", channel.name)),
+        )
+        .await?;
+    }else { 
+        ctx.send(create_ephemeral_reply("当前未加入任何一个语音频道")).await?;
     }
-    ctx.send(
-        CreateReply::default()
-            .ephemeral(true)
-            .content(format!("已离开频道 {}", channel.name)),
-    )
-    .await?;
+
     Ok(())
 }
 
